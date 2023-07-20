c9f43032fa3b2822394e4c7b76367f0219ee0658
Sanjid Rahman <sanjid.rahman@servian.com>
2023-07-20 20:02:54 +1200

    prepare for home row mods with tapping term

--- keyboards/cantor/config.h
+++ keyboards/cantor/config.h
@@ -7,6 +7,14 @@
 #define SERIAL_USART_TX_PIN B6     // USART TX pin
 #define SERIAL_USART_RX_PIN B7     // USART RX pin
 
+
+// default but used in macros
+#undef TAPPING_TERM
+#define TAPPING_TERM 300
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 0
+
 /*
  * Feature disable options
  *  These options are also useful to firmware size reduction.
